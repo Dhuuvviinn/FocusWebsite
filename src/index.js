f278a7b673ffb456ac1635635b091388abc4f2d7
dhruvin <dhruvinpatel17000@gmail.com>
2024-01-01 12:50:35 +0530

    ProductionSetup

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ dotenv.config({
     path:"./env"
 })
 
-
 connectDB()
+
 .then(()=>{
     app.listen(process.env.PORT || 8000,()=>{
         console.log(`Server is connected at Port:${process.env.PORT}`)
